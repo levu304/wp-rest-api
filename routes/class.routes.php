d357df4ecd89f995e324759fee750991c95652fc
levu304 <levu3041988@gmail.com>
2020-11-10 11:02:20 +0700

    fix cors

--- routes/class.routes.php
+++ routes/class.routes.php
@@ -16,7 +16,19 @@ class Wordpress_REST_API {
      * @return void
      */
     public function init() {
-        header("Access-Control-Allow-Origin: *");
+
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+        header('Content-Type: application/json');
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method == "OPTIONS") {
+            header('Access-Control-Allow-Origin: *');
+            header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+            header("HTTP/1.1 200 OK");
+            die();
+        }
+        
         if (!function_exists('register_rest_route')) {
             return false;
         }
